47257cbcdc0c5ec273910302978345acea865a75
polltooh <polltooh@gmail.com>
2017-02-19 21:00:22 +0800

    add length variable

--- gen_list_script/gen_train_list.py
+++ gen_list_script/gen_train_list.py
@@ -18,15 +18,18 @@ def file_list_to_list(file_list, unroll_num):
     return new_list 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        data_dir = sys.argv[1]
-    else:
-        data_dir = "../../data"
+    if not len(sys.argv) == 3:
+        print("Usage: gen_train_list.py data_dir size_len")
+        exit(1)
+
+    data_dir = sys.argv[1]
+    size_len = int(sys.argv[2])
+    dsize = (size_len, size_len)
 
     file_list_dir = "../file_list/"
-    data_ext = "_resize.jpg"
-    label_ext = "_resize.desmap"
-    mask_ext = "_msk_resize.npy"    
+    data_ext = "_%d.jpg"%size_len
+    label_ext = "_%d.desmap"%size_len
+    mask_ext = "_msk_%d.npy"%size_len
 
     unroll_num = 5
 
