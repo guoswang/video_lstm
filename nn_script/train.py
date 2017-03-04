9c81f01a2a166c289f3c3e56a50bcc02d69f00b9
polltooh <polltooh@gmail.com>
2017-03-04 19:57:33 +0800

    add input model proto

--- nn_script/train.py
+++ nn_script/train.py
@@ -3,6 +3,11 @@ from TensorflowToolbox.utility import file_io
 from net_flow import NetFlow
 
 if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        model_proto_file = sys.argv[1]
+    else:
+        model_proto_file = "model.tfproto"
+
     model_params = rp.load_proto("model.tfproto")
     file_io.check_exist(model_params["train_file_name"]) 
     file_io.check_exist(model_params["test_file_name"]) 
