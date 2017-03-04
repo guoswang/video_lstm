39b343908cd968ea693febfa30d2fa0a22401495
polltooh <polltooh@gmail.com>
2017-03-04 19:58:36 +0800

    add sys

--- nn_script/train.py
+++ nn_script/train.py
@@ -1,6 +1,7 @@
 from TensorflowToolbox.utility import read_proto as rp
 from TensorflowToolbox.utility import file_io
 from net_flow import NetFlow
+import sys
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
