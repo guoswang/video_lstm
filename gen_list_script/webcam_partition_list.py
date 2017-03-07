37d91ed684b3c261768f6c3c84cb807a4d5600ed
polltooh <polltooh@gmail.com>
2017-03-07 08:41:19 -0500

    change data localtion

--- gen_list_script/webcam_partition_list.py
+++ gen_list_script/webcam_partition_list.py
@@ -27,8 +27,8 @@ def check_add_to_list(file_dict, file_list, file_name, full_name):
     return file_list
 
 if __name__ == "__main__":
-    train_list = file_io.read_file("../file_list/train_list2.txt")
-    test_list = file_io.read_file("../file_list/test_list2.txt")
+    train_list = file_io.read_file("../file_list/train_list1.txt")
+    test_list = file_io.read_file("../file_list/test_list1.txt")
     full_list = train_list + test_list
 
     file_list_dir = "../file_list/"
